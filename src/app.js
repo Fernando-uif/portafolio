4da55da2de233f776753422c1781dcdb2bcf6869
fernando-ui <fst-96@hotmail.com>
2022-08-06 14:19:59 -0500

    Responsive hasta los 700px

--- src/app.js
+++ src/app.js
@@ -107,3 +107,21 @@ window.addEventListener("scroll", () => {
     }
     ;
 });
+window.addEventListener("scroll", () => {
+    const listNode = document.querySelectorAll(".tecnologies__card-7") || document.createElement("div");
+    const firstElement = listNode[0];
+    const position = firstElement.getBoundingClientRect().top;
+    const tamanioPantalla = window.innerHeight / 2;
+    for (let i = 0; i < listNode.length; i++) {
+        let element = listNode[i];
+        if (position < tamanioPantalla) {
+            element.classList.add("tecnologies__fade_in");
+            element.classList.remove("tecnologies__fade_out");
+        }
+        else {
+            element.classList.remove("tecnologies__fade_in");
+            element.classList.add("tecnologies__fade_out");
+        }
+    }
+    ;
+});
