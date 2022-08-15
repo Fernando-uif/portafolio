0aa6b1a8d6af9f257c19c0a65e97e5ceacced1ea
fernando-ui <fst-96@hotmail.com>
2022-08-15 16:33:11 -0500

    Agreagando descripcion y arreglando botn de descarga

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 "use strict";
-window.addEventListener("scroll", () => {
-    const listNode = document.querySelectorAll(".tecnologies__card-1") || document.createElement("div");
-    const firstElement = listNode[0];
-    const position = firstElement.getBoundingClientRect().top;
-    const tamanioPantalla = window.innerHeight / 2;
-    for (let i = 0; i < listNode.length; i++) {
-        let element = listNode[i];
+window.addEventListener("scroll", function () {
+    var listNode = document.querySelectorAll(".tecnologies__card-1") ||
+        document.createElement("div");
+    var firstElement = listNode[0];
+    var position = firstElement.getBoundingClientRect().top;
+    var tamanioPantalla = window.innerHeight / 2;
+    for (var i = 0; i < listNode.length; i++) {
+        var element = listNode[i];
         if (position < tamanioPantalla) {
             element.classList.add("tecnologies__fade_in");
             element.classList.remove("tecnologies__fade_out");
@@ -15,15 +16,15 @@ window.addEventListener("scroll", () => {
             element.classList.add("tecnologies__fade_out");
         }
     }
-    ;
 });
-window.addEventListener("scroll", () => {
-    const listNode = document.querySelectorAll(".tecnologies__card-2") || document.createElement("div");
-    const firstElement = listNode[0];
-    const position = firstElement.getBoundingClientRect().top;
-    const tamanioPantalla = window.innerHeight / 2;
-    for (let i = 0; i < listNode.length; i++) {
-        let element = listNode[i];
+window.addEventListener("scroll", function () {
+    var listNode = document.querySelectorAll(".tecnologies__card-2") ||
+        document.createElement("div");
+    var firstElement = listNode[0];
+    var position = firstElement.getBoundingClientRect().top;
+    var tamanioPantalla = window.innerHeight / 2;
+    for (var i = 0; i < listNode.length; i++) {
+        var element = listNode[i];
         if (position < tamanioPantalla) {
             element.classList.add("tecnologies__fade_in");
             element.classList.remove("tecnologies__fade_out");
@@ -33,15 +34,15 @@ window.addEventListener("scroll", () => {
             element.classList.add("tecnologies__fade_out");
         }
     }
-    ;
 });
-window.addEventListener("scroll", () => {
-    const listNode = document.querySelectorAll(".tecnologies__card-3") || document.createElement("div");
-    const firstElement = listNode[0];
-    const position = firstElement.getBoundingClientRect().top;
-    const tamanioPantalla = window.innerHeight / 2;
-    for (let i = 0; i < listNode.length; i++) {
-        let element = listNode[i];
+window.addEventListener("scroll", function () {
+    var listNode = document.querySelectorAll(".tecnologies__card-3") ||
+        document.createElement("div");
+    var firstElement = listNode[0];
+    var position = firstElement.getBoundingClientRect().top;
+    var tamanioPantalla = window.innerHeight / 2;
+    for (var i = 0; i < listNode.length; i++) {
+        var element = listNode[i];
         if (position < tamanioPantalla) {
             element.classList.add("tecnologies__fade_in");
             element.classList.remove("tecnologies__fade_out");
@@ -51,15 +52,15 @@ window.addEventListener("scroll", () => {
             element.classList.add("tecnologies__fade_out");
         }
     }
-    ;
 });
-window.addEventListener("scroll", () => {
-    const listNode = document.querySelectorAll(".tecnologies__card-4") || document.createElement("div");
-    const firstElement = listNode[0];
-    const position = firstElement.getBoundingClientRect().top;
-    const tamanioPantalla = window.innerHeight / 2;
-    for (let i = 0; i < listNode.length; i++) {
-        let element = listNode[i];
+window.addEventListener("scroll", function () {
+    var listNode = document.querySelectorAll(".tecnologies__card-4") ||
+        document.createElement("div");
+    var firstElement = listNode[0];
+    var position = firstElement.getBoundingClientRect().top;
+    var tamanioPantalla = window.innerHeight / 2;
+    for (var i = 0; i < listNode.length; i++) {
+        var element = listNode[i];
         if (position < tamanioPantalla) {
             element.classList.add("tecnologies__fade_in");
             element.classList.remove("tecnologies__fade_out");
@@ -69,15 +70,15 @@ window.addEventListener("scroll", () => {
             element.classList.add("tecnologies__fade_out");
         }
     }
-    ;
 });
-window.addEventListener("scroll", () => {
-    const listNode = document.querySelectorAll(".tecnologies__card-5") || document.createElement("div");
-    const firstElement = listNode[0];
-    const position = firstElement.getBoundingClientRect().top;
-    const tamanioPantalla = window.innerHeight / 2;
-    for (let i = 0; i < listNode.length; i++) {
-        let element = listNode[i];
+window.addEventListener("scroll", function () {
+    var listNode = document.querySelectorAll(".tecnologies__card-5") ||
+        document.createElement("div");
+    var firstElement = listNode[0];
+    var position = firstElement.getBoundingClientRect().top;
+    var tamanioPantalla = window.innerHeight / 2;
+    for (var i = 0; i < listNode.length; i++) {
+        var element = listNode[i];
         if (position < tamanioPantalla) {
             element.classList.add("tecnologies__fade_in");
             element.classList.remove("tecnologies__fade_out");
@@ -87,15 +88,15 @@ window.addEventListener("scroll", () => {
             element.classList.add("tecnologies__fade_out");
         }
     }
-    ;
 });
-window.addEventListener("scroll", () => {
-    const listNode = document.querySelectorAll(".tecnologies__card-6") || document.createElement("div");
-    const firstElement = listNode[0];
-    const position = firstElement.getBoundingClientRect().top;
-    const tamanioPantalla = window.innerHeight / 2;
-    for (let i = 0; i < listNode.length; i++) {
-        let element = listNode[i];
+window.addEventListener("scroll", function () {
+    var listNode = document.querySelectorAll(".tecnologies__card-6") ||
+        document.createElement("div");
+    var firstElement = listNode[0];
+    var position = firstElement.getBoundingClientRect().top;
+    var tamanioPantalla = window.innerHeight / 2;
+    for (var i = 0; i < listNode.length; i++) {
+        var element = listNode[i];
         if (position < tamanioPantalla) {
             element.classList.add("tecnologies__fade_in");
             element.classList.remove("tecnologies__fade_out");
@@ -105,15 +106,15 @@ window.addEventListener("scroll", () => {
             element.classList.add("tecnologies__fade_out");
         }
     }
-    ;
 });
-window.addEventListener("scroll", () => {
-    const listNode = document.querySelectorAll(".tecnologies__card-7") || document.createElement("div");
-    const firstElement = listNode[0];
-    const position = firstElement.getBoundingClientRect().top;
-    const tamanioPantalla = window.innerHeight / 2;
-    for (let i = 0; i < listNode.length; i++) {
-        let element = listNode[i];
+window.addEventListener("scroll", function () {
+    var listNode = document.querySelectorAll(".tecnologies__card-7") ||
+        document.createElement("div");
+    var firstElement = listNode[0];
+    var position = firstElement.getBoundingClientRect().top;
+    var tamanioPantalla = window.innerHeight / 2;
+    for (var i = 0; i < listNode.length; i++) {
+        var element = listNode[i];
         if (position < tamanioPantalla) {
             element.classList.add("tecnologies__fade_in");
             element.classList.remove("tecnologies__fade_out");
@@ -123,5 +124,10 @@ window.addEventListener("scroll", () => {
             element.classList.add("tecnologies__fade_out");
         }
     }
-    ;
 });
+// document.querySelector('#downloadButton');
+// document.querySelector('#downloadButton')?.addEventListener('click',()=>{
+//   console.log('click');
+//   document.querySelector('#downloadAnchor')?.click();
+// }
+// )
--- src/app.ts
+++ src/app.ts
@@ -1,127 +1,143 @@
+
 window.addEventListener("scroll", () => {
-  const listNode:NodeList =    document.querySelectorAll(".tecnologies__card-1") || document.createElement("div");
-  const firstElement:HTMLElement = listNode[0] as HTMLElement;
+  const listNode: NodeList =
+    document.querySelectorAll(".tecnologies__card-1") ||
+    document.createElement("div");
+  const firstElement: HTMLElement = listNode[0] as HTMLElement;
   const position = firstElement.getBoundingClientRect().top;
   const tamanioPantalla = window.innerHeight / 2;
 
-  for(let i = 0; i < listNode.length; i++){
-    let element:HTMLElement = listNode[i] as HTMLElement;
-    if(position < tamanioPantalla){
+  for (let i = 0; i < listNode.length; i++) {
+    let element: HTMLElement = listNode[i] as HTMLElement;
+    if (position < tamanioPantalla) {
       element.classList.add("tecnologies__fade_in");
       element.classList.remove("tecnologies__fade_out");
-    }else{
+    } else {
       element.classList.remove("tecnologies__fade_in");
       element.classList.add("tecnologies__fade_out");
-    }
-
-  };
-});
+    } 
+  }
+}); 
 
 window.addEventListener("scroll", () => {
-  const listNode:NodeList =    document.querySelectorAll(".tecnologies__card-2") || document.createElement("div");
-  const firstElement:HTMLElement = listNode[0] as HTMLElement;
+  const listNode: NodeList =
+    document.querySelectorAll(".tecnologies__card-2") ||
+    document.createElement("div");
+  const firstElement: HTMLElement = listNode[0] as HTMLElement;
   const position = firstElement.getBoundingClientRect().top;
   const tamanioPantalla = window.innerHeight / 2;
 
-  for(let i = 0; i < listNode.length; i++){
-    let element:HTMLElement = listNode[i] as HTMLElement;
-    if(position < tamanioPantalla){
+  for (let i = 0; i < listNode.length; i++) {
+    let element: HTMLElement = listNode[i] as HTMLElement;
+    if (position < tamanioPantalla) {
       element.classList.add("tecnologies__fade_in");
       element.classList.remove("tecnologies__fade_out");
-    }else{
+    } else {
       element.classList.remove("tecnologies__fade_in");
       element.classList.add("tecnologies__fade_out");
     }
-
-  };
+  }
 });
 window.addEventListener("scroll", () => {
-  const listNode:NodeList =    document.querySelectorAll(".tecnologies__card-3") || document.createElement("div");
-  const firstElement:HTMLElement = listNode[0] as HTMLElement;
+  const listNode: NodeList =
+    document.querySelectorAll(".tecnologies__card-3") ||
+    document.createElement("div");
+  const firstElement: HTMLElement = listNode[0] as HTMLElement;
   const position = firstElement.getBoundingClientRect().top;
   const tamanioPantalla = window.innerHeight / 2;
 
-  for(let i = 0; i < listNode.length; i++){
-    let element:HTMLElement = listNode[i] as HTMLElement;
-    if(position < tamanioPantalla){
+  for (let i = 0; i < listNode.length; i++) {
+    let element: HTMLElement = listNode[i] as HTMLElement;
+    if (position < tamanioPantalla) {
       element.classList.add("tecnologies__fade_in");
       element.classList.remove("tecnologies__fade_out");
-    }else{
+    } else {
       element.classList.remove("tecnologies__fade_in");
       element.classList.add("tecnologies__fade_out");
     }
-
-  };
+  }
 });
 window.addEventListener("scroll", () => {
-  const listNode:NodeList =    document.querySelectorAll(".tecnologies__card-4") || document.createElement("div");
-  const firstElement:HTMLElement = listNode[0] as HTMLElement;
+  const listNode: NodeList =
+    document.querySelectorAll(".tecnologies__card-4") ||
+    document.createElement("div");
+  const firstElement: HTMLElement = listNode[0] as HTMLElement;
   const position = firstElement.getBoundingClientRect().top;
   const tamanioPantalla = window.innerHeight / 2;
 
-  for(let i = 0; i < listNode.length; i++){
-    let element:HTMLElement = listNode[i] as HTMLElement;
-    if(position < tamanioPantalla){
+  for (let i = 0; i < listNode.length; i++) {
+    let element: HTMLElement = listNode[i] as HTMLElement;
+    if (position < tamanioPantalla) {
       element.classList.add("tecnologies__fade_in");
       element.classList.remove("tecnologies__fade_out");
-    }else{
+    } else {
       element.classList.remove("tecnologies__fade_in");
       element.classList.add("tecnologies__fade_out");
     }
-
-  };
+  }
 });
 window.addEventListener("scroll", () => {
-  const listNode:NodeList =    document.querySelectorAll(".tecnologies__card-5") || document.createElement("div");
-  const firstElement:HTMLElement = listNode[0] as HTMLElement;
+  const listNode: NodeList =
+    document.querySelectorAll(".tecnologies__card-5") ||
+    document.createElement("div");
+  const firstElement: HTMLElement = listNode[0] as HTMLElement;
   const position = firstElement.getBoundingClientRect().top;
   const tamanioPantalla = window.innerHeight / 2;
 
-  for(let i = 0; i < listNode.length; i++){
-    let element:HTMLElement = listNode[i] as HTMLElement;
-    if(position < tamanioPantalla){
+  for (let i = 0; i < listNode.length; i++) {
+    let element: HTMLElement = listNode[i] as HTMLElement;
+    if (position < tamanioPantalla) {
       element.classList.add("tecnologies__fade_in");
       element.classList.remove("tecnologies__fade_out");
-    }else{
+    } else {
       element.classList.remove("tecnologies__fade_in");
       element.classList.add("tecnologies__fade_out");
     }
-
-  };
+  }
 });
 window.addEventListener("scroll", () => {
-  const listNode:NodeList =    document.querySelectorAll(".tecnologies__card-6") || document.createElement("div");
-  const firstElement:HTMLElement = listNode[0] as HTMLElement;
+  const listNode: NodeList =
+    document.querySelectorAll(".tecnologies__card-6") ||
+    document.createElement("div");
+  const firstElement: HTMLElement = listNode[0] as HTMLElement;
   const position = firstElement.getBoundingClientRect().top;
   const tamanioPantalla = window.innerHeight / 2;
 
-  for(let i = 0; i < listNode.length; i++){
-    let element:HTMLElement = listNode[i] as HTMLElement;
-    if(position < tamanioPantalla){
+  for (let i = 0; i < listNode.length; i++) {
+    let element: HTMLElement = listNode[i] as HTMLElement;
+    if (position < tamanioPantalla) {
       element.classList.add("tecnologies__fade_in");
       element.classList.remove("tecnologies__fade_out");
-    }else{
+    } else {
       element.classList.remove("tecnologies__fade_in");
       element.classList.add("tecnologies__fade_out");
     }
-
-  };
+  }
 });
 window.addEventListener("scroll", () => {
-  const listNode:NodeList =    document.querySelectorAll(".tecnologies__card-7") || document.createElement("div");
-  const firstElement:HTMLElement = listNode[0] as HTMLElement;
+  const listNode: NodeList =
+    document.querySelectorAll(".tecnologies__card-7") ||
+    document.createElement("div");
+  const firstElement: HTMLElement = listNode[0] as HTMLElement;
   const position = firstElement.getBoundingClientRect().top;
   const tamanioPantalla = window.innerHeight / 2;
 
-  for(let i = 0; i < listNode.length; i++){
-    let element:HTMLElement = listNode[i] as HTMLElement;
-    if(position < tamanioPantalla){
+  for (let i = 0; i < listNode.length; i++) {
+    let element: HTMLElement = listNode[i] as HTMLElement;
+    if (position < tamanioPantalla) {
       element.classList.add("tecnologies__fade_in");
       element.classList.remove("tecnologies__fade_out");
-    }else{
+    } else {
       element.classList.remove("tecnologies__fade_in");
       element.classList.add("tecnologies__fade_out");
     }
-
-  };
+  }
 });
+
+
+// document.querySelector('#downloadButton');
+// document.querySelector('#downloadButton')?.addEventListener('click',()=>{
+//   console.log('click');
+//   document.querySelector('#downloadAnchor')?.click();
+// }
+// )
